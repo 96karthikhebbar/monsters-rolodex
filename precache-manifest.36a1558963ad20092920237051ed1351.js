ea45b5e213f7aadcaef64b543dc60abdfcd26c72
Karthik  Hebbar <96karthikhebbar@gmail.com>
2020-10-25 16:11:41 +0530

    Updates

---
+++ precache-manifest.36a1558963ad20092920237051ed1351.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a13a987e4c320c75abdc89b6f122db1e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c469d045528bf37d0acc",
+    "url": "/monsters-rolodex/static/css/main.19858985.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c469d045528bf37d0acc",
+    "url": "/monsters-rolodex/static/js/main.e3a6b8f2.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
